154f23f3a9280700f1b06994817ed295793e4031
shimehituzi <shimehituzi@gmail.com>
2021-09-13 20:57:38 +0900

    エンコーダーを切り分けた

---
+++ cmd/pccbi/encoder.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/shimehituzi/pccbi/internal/codec"
+	"github.com/shimehituzi/pccbi/internal/processing"
+)
+
+func encode() {
+	relativePath := "../../DATABASE/orig/soldier/soldier/Ply/soldier_vox10_0537.ply"
+	// 1062090 点のデータ
+	srcPath := relativePath[4:]
+	voxel, err := processing.LoadPly(srcPath, processing.YZX.Order())
+	if err != nil {
+		panic(err)
+	}
+	cb := processing.NewContourBuffer(voxel)
+	stream := processing.NewStream(voxel, cb)
+	codec.Encode(stream)
+}
