0a903877844d2786e19e3a78b2352f612e936ad8
shimehituzi <shimehituzi@gmail.com>
2021-12-11 08:36:33 +0900

    後処理

--- internal/tool/preprocessing.go
+++ internal/tool/preprocessing.go
@@ -1,85 +1,19 @@
 package tool
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"sort"
-	"strconv"
-	"strings"
-)
-
-type plyHeader []string
-type plyData [][]int
+import "sort"
 
 func Preprocessing(srcPath, sortedPath, etcPath string) {
-	data, header := readSortedPly(srcPath)
-	writePly(sortedPath, data, header, true)
-	writePly(etcPath, data, header, false)
-}
-
-func writePly(path string, data plyData, header plyHeader, all bool) {
-	fp, err := os.Create(path)
-	if err != nil {
-		panic(err)
-	}
-	defer fp.Close()
-
-	w := bufio.NewWriter(fp)
-
-	for _, v := range header {
-		w.WriteString(v + "\n")
-	}
-
-	for i := range data {
-		var line []int
-		if all {
-			line = data[i]
-		} else {
-			line = data[i][3:]
-		}
-		text := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(line)), " "), "[]")
-		w.WriteString(text + "\n")
-	}
-
-	w.Flush()
-}
-
-func readSortedPly(srcPath string) (plyData, plyHeader) {
-	fp, err := os.Open(srcPath)
-	if err != nil {
-		panic(err)
-	}
-	defer fp.Close()
-
-	sccaner := bufio.NewScanner(fp)
-
-	header := plyHeader{}
-	data := plyData{}
-	for isData := false; sccaner.Scan(); {
-		text := sccaner.Text()
-		if isData {
-			texts := strings.Split(text, " ")
-			line := make([]int, len(texts))
-			for i := range line {
-				line[i], err = strconv.Atoi(texts[i])
-				if err != nil {
-					panic(err)
-				}
-			}
-			data = append(data, line)
-		} else {
-			header = append(header, text)
-		}
-
-		if text == "end_header" {
-			isData = true
-		}
-	}
+	data, header := readPly(srcPath)
 
 	sort.Sort(data)
 
-	return data, header
+	writePly(sortedPath, data, header)
+
+	etcData := make(plyData, len(data))
+	for i := range etcData {
+		etcData[i] = data[i][3:]
+	}
+	writePly(etcPath, etcData, header)
 }
 
 func (data plyData) Len() int { return len(data) }
